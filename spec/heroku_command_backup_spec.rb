78e5795d0793820c9c35b5d19a5c29270933bd94
Dan Porter <wolfpakz@gmail.com>
2011-02-14 20:44:35 -0800

    Added a few more tests

--- spec/heroku_command_backup_spec.rb
+++ spec/heroku_command_backup_spec.rb
@@ -7,6 +7,26 @@ describe Heroku::Command::Backup do
     @backup = Heroku::Command::Backup.new(['--app', @app])
   end
 
+  describe "addons" do
+    before(:each) do
+      addons = [
+        {"configured" => true, "name" => "configey"},
+        {"configured" => false, "name" => "noconf"},
+        {"configured" => true, "name" => "configey2"},
+        {"configured" => false, "name" => "noconf2"}
+      ]
+      Heroku::Client.any_instance.expects(:installed_addons).returns(addons)
+    end
+    
+    it "should return configured addons" do
+      @backup.addons.all?{|a| a['configured'] }.should be_true
+    end
+
+    it "should not return addons which aren't configured" do
+      @backup.addons.any?{|a| a['configure'] == false}.should be_false
+    end
+  end
+
   describe "bundles_addon_installed?" do
     it "should return true if bundles:single is installed" do
       addons = [{"name"=>"bundles:single", "description"=>"Single Bundle", "configured"=>true}]
@@ -24,6 +44,13 @@ describe Heroku::Command::Backup do
       addons = [{"name"=>"redis:basic", "description"=>"Redis", "configured"=>true}]
       @backup.expects(:addons).returns(addons)
       @backup.bundles_addon_installed?.should be_false
+    end
+  end
+
+  describe "download_file_name" do
+    it "should be based on the backup finish time" do
+      @backup.expects(:latest_backup).returns({"finished_at" => "2009/11/02 12:15:32"})
+      @backup.download_file_name.should == "2009-11-02-1215.dump"
     end
   end
 
